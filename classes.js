740b9d69db04dba4361bde31a291e62984e7a653
Aaron Latham <latham91@icloud.com>
2024-01-15 14:18:56 +0000

    refined function call

--- classes.js
+++ classes.js
@@ -23,6 +23,7 @@ class CoffeeShop {
     order(drink) {
         this.orderList.push(drink); // add drink to order array.
         this.total += this.drinks[drink]; // add drink price to total.
+        return this;
     }
 
     completeOrder() {
@@ -52,8 +53,5 @@ class Person {
 const till = new CoffeeShop(); // create new coffeeshop instance.
 const customer = new Person("Aaron", 5.0); // create new person instance with their name and balance.
 
-till.order("Coke");
-till.order("Coffee");
-till.order("Fanta");
-till.order("Water");
+till.order("Coke").order("Coffee").order("Fanta").order("Water"); // order drinks.
 customer.checkBalance(till.completeOrder());
